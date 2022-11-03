78881d245718c0557a12f17597953b5bc08eba69
董士崇 <dongshichong@thape.com.cn>
2022-11-03 09:57:33 +0800

    Add ThBimBeam and ThBimColumn

---
+++ THBimEngine.Domain/THBimBeam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace THBimEngine.Domain
+{
+    public class THBimBeam : THBimEntity, IEquatable<THBimBeam>
+    {
+        public THBimBeam(int id, string name, string material, GeometryParam geometryParam, string describe = "", string uid = "") : base(id, name, material, geometryParam, describe, uid)
+        {
+            //
+        }
+
+        public override object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(THBimBeam other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ THBimEngine.Domain/THBimColumn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace THBimEngine.Domain
+{
+    public class THBimColumn : THBimEntity, IEquatable<THBimColumn>
+    {
+        public THBimColumn(int id, string name, string material, GeometryParam geometryParam, string describe = "", string uid = "") : base(id, name, material, geometryParam, describe, uid)
+        {
+        }
+
+        public override object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(THBimColumn other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

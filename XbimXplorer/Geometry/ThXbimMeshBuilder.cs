e992a61f27242fdbfe7a6ecf7aab5a20e58d6431
董士崇 <dongshichong@thape.com.cn>
2022-11-18 14:17:42 +0800

    Implement ThXbimMeshBuilder

---
+++ XbimXplorer/Geometry/ThXbimMeshBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xbim.Geom;
+using Xbim.Common.Geometry;
+
+namespace XbimXplorer.Geometry
+{
+    public class ThXbimMeshBuilder
+    {
+        private IGeometryStoreReader Reader { get; set; }
+
+        public ThXbimMeshBuilder(IGeometryStoreReader reader)
+        {
+            Reader = reader;
+        }
+
+        public XbimMesher ToMesh(XbimShapeInstance shapeInstance)
+        {
+            IXbimShapeGeometryData shapeGeom = Reader.ShapeGeometry(shapeInstance.ShapeGeometryLabel);
+            if (shapeGeom.Format != (byte)XbimGeometryType.PolyhedronBinary)
+            {
+                throw new NotSupportedException();
+            }
+
+            var xbimMesher = new XbimMesher();
+            xbimMesher.AddMesh(shapeGeom.ShapeData);
+            return xbimMesher;
+        }
+    }
+}

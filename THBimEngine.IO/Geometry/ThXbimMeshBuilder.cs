6fc14abcf7615bebb6ed496913b9f43b4d688d2a
董士崇 <dongshichong@thape.com.cn>
2022-11-19 20:40:03 +0800

    XbimMesher to wrap mesh data

---
+++ THBimEngine.IO/Geometry/ThXbimMeshBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xbim.Geom;
+using Xbim.Common.Geometry;
+
+namespace XbimXplorer.Geometry
+{
+    public class ThXbimMeshBuilder
+    {
+        private IGeometryStoreReader Reader { get; set; }
+
+        public ThXbimMeshBuilder(IGeometryStoreReader reader)
+        {
+            Reader = reader;
+        }
+
+        public XbimMesher ToMesh(XbimShapeInstance shapeInstance)
+        {
+            // Reference:
+            //  https://github.com/xBimTeam/XbimGltf/blob/master/Xbim.GLTF.IO/Builder.cs
+            var shapeGeom = Reader.ShapeGeometryOfInstance(shapeInstance);
+            if (shapeGeom.Format != XbimGeometryType.PolyhedronBinary)
+            {
+                throw new NotSupportedException();
+            }
+
+            var xbimMesher = new XbimMesher();
+            xbimMesher.AddMesh((shapeGeom as IXbimShapeGeometryData).ShapeData);
+            return xbimMesher;
+        }
+    }
+}

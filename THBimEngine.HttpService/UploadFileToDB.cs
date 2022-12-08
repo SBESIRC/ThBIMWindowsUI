e8eb9309e9823650f04fee03dfdb4213d0bb4526
zyk-work <81728112+zyk-work@users.noreply.github.com>
2022-12-08 11:04:59 +0800

    Feature/上传DB加入上传数据到协同平台 (#27)
    * 临时提交
    
    * 临时提交
    
    * 上传ifc到db文件，临时提交，还有问题调整
    
    * 上传数据到DB平台的功能加入
    
    * 临时提交
    
    Co-authored-by: 赵玉康 <zhaoyukang@thape.com.cn>

---
+++ THBimEngine.HttpService/UploadFileToDB.cs
@@ -0,0 +1,154 @@
+﻿using LBimFileCommit;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using THBimEngine.Common;
+
+namespace THBimEngine.HttpService
+{
+    public class UploadFileToDB
+    {
+        string projectUrl = "http://81.69.195.121:8888/Portal/Api/ProejctController/GetProjects";
+        string subProjectUrl = "http://81.69.195.121:8888/Portal/Api/ProejctController/GetSubentries";
+        string projectFileUrl = "http://81.69.195.121:8888/Portal/Api/ProejctController/GetModelFiles";
+        public List<DBProjectInfo> GetDBProject()
+        {
+            var dbProjects = new List<DBProjectInfo>();
+            var client = new RestClient();
+            var request = new RestRequest(new Uri(projectUrl, UriKind.RelativeOrAbsolute), Method.Get);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+            if (null != request && !string.IsNullOrEmpty(response.Content))
+            {
+                var res = JsonHelper.DeserializeJsonToObject<ResInfo>(response.Content);
+                if (res.succ)
+                    dbProjects = JsonHelper.DeserializeJsonToList<DBProjectInfo>(((Newtonsoft.Json.Linq.JContainer)res.Data).First().First().ToString());
+            }
+            return dbProjects;
+        }
+        public List<DBSubProjectInfo> GetSubProjects(string prjId) 
+        {
+            var dbSubProjects = new List<DBSubProjectInfo>();
+            var client = new RestClient();
+            var request = new RestRequest(subProjectUrl, Method.Get);
+            request.AddParameter("prjId", prjId, ParameterType.QueryString,false);
+            var response = client.Execute(request);
+            if (null != request && !string.IsNullOrEmpty(response.Content))
+            {
+                try
+                {
+                    var res = JsonHelper.DeserializeJsonToObject<ResInfo>(response.Content);
+                    if (res.succ)
+                        dbSubProjects = JsonHelper.DeserializeJsonToList<DBSubProjectInfo>(((Newtonsoft.Json.Linq.JContainer)res.Data).First().First().ToString());
+                }
+                catch { }
+            }
+            return dbSubProjects;
+        }
+        public List<DBProjectFileInfo> GetSubProjectFiles(string subPrjId) 
+        {
+            var dbFiles = new List<DBProjectFileInfo>();
+            var client = new RestClient();
+            var request = new RestRequest(projectFileUrl, Method.Get);
+            request.AddParameter("subId", subPrjId, ParameterType.QueryString, false);
+            var response = client.Execute(request);
+            if (null != request && !string.IsNullOrEmpty(response.Content))
+            {
+                try
+                {
+                    var res = JsonHelper.DeserializeJsonToObject<ResInfo>(response.Content);
+                    if (res.succ)
+                        dbFiles = JsonHelper.DeserializeJsonToList<DBProjectFileInfo>(((Newtonsoft.Json.Linq.JContainer)res.Data).First().First().ToString());
+                }
+                catch { }
+            }
+            return dbFiles;
+        }
+
+        public string FileUploadToDB(DBProjectInfo projectInfo, DBSubProjectInfo subProjectInfo, DBProjectFileInfo fileInfo, string filePath) 
+        {
+            var fileName = Path.GetFileNameWithoutExtension(filePath);
+            var args = new CommitArguments()
+            {
+                PrjId = projectInfo.Id,
+                PrjName = projectInfo.PrjName,
+                SubId = subProjectInfo.Id,
+                SubName = subProjectInfo.SubEntryName,
+                ModelId = fileInfo.ModelId,
+                ModelName = fileName,
+                FileId = fileInfo.Id,
+                VerNo = fileInfo.VersionNo,
+                ModelSubName = "默认",
+                Major = MajorCodeToEMajor(fileInfo.Major),
+                IfcFilePath = filePath,
+                IsUpdate = false
+            };
+            var result = FileManager.Commit(args);
+            return result.IsSucc ? "" : string.IsNullOrEmpty(result.ErrMessage) ? "上传失败" : result.ErrMessage;
+        }
+        public string FileUpdateToDB(DBProjectInfo projectInfo, DBSubProjectInfo subProjectInfo, DBProjectFileInfo fileInfo, string filePath)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(filePath);
+            var args = new CommitArguments()
+            {
+                PrjId = projectInfo.Id,
+                PrjName = projectInfo.PrjName,
+                SubId = subProjectInfo.Id,
+                SubName = subProjectInfo.SubEntryName,
+                ModelId = fileInfo.ModelId,
+                ModelName = fileName,
+                FileId = fileInfo.Id,
+                VerNo = fileInfo.VersionNo,
+                ModelSubName = "",
+                Major = MajorCodeToEMajor(fileInfo.Major),
+                IfcFilePath = filePath,
+                IsUpdate = true
+            };
+            var result = FileManager.Commit(args);
+            return result.IsSucc ? "" : string.IsNullOrEmpty(result.ErrMessage) ? "上传失败" : result.ErrMessage;
+        }
+        public MajorEnum MajorCodeToEMajor(string majorCode) 
+        {
+            var res = MajorEnum.建筑;
+            switch (majorCode.ToUpper()) 
+            {
+                case "A":
+                    res = MajorEnum.建筑;
+                    break;
+                case "S":
+                    res = MajorEnum.结构;
+                    break;
+            }
+            return res;
+        }
+    }
+    public class DBProjectFileInfo 
+    {
+        public string Id { get; set; }
+        public string ModelId { get; set; }
+        public string FileName { get; set; }
+        public string Major { get; set; }
+        public string FileType { get; set; }
+        public string DesignSubentryNames { get; set; }
+        public string VersionNo { get; set; }
+    }
+    public class DBProjectInfo 
+    {
+        public string Id { get; set; }
+        public string PrjNo { get; set; }
+        public string PrjName { get; set; }
+    }
+    public class DBSubProjectInfo 
+    {
+        public string Id { get; set; }
+        public string SubEntryName { get; set; }
+    }
+    public class ResInfo 
+    {
+        public bool succ { get; set; }
+        public object err { get; set; }
+        public object Data { get; set; }
+    }
+}
---
+++ XbimXplorer/UploadDBFile.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using THBimEngine.Application;
+using THBimEngine.HttpService;
+
+namespace XbimXplorer
+{
+    /// <summary>
+    /// UploadDBFile.xaml 的交互逻辑
+    /// </summary>
+    public partial class UploadDBFile : Window
+    {
+        SelectUploadFileVM uploadFileVM;
+        public UploadDBFile(List<DBProjectFileInfo> projectFileInfos)
+        {
+            InitializeComponent();
+            uploadFileVM = new SelectUploadFileVM(projectFileInfos);
+            this.DataContext = uploadFileVM;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+        public DBProjectFileInfo GetSelectFile() 
+        {
+            if (null == uploadFileVM || uploadFileVM.SelectFile == null)
+                return null;
+            return uploadFileVM.SelectFile.FileInfo;
+        }
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (null == uploadFileVM || uploadFileVM.SelectFile == null)
+                return;
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+    class SelectUploadFileVM : NotifyPropertyChangedBase
+    {
+        private ObservableCollection<DBFileVM> allFiles { get; set; }
+        public ObservableCollection<DBFileVM> AllFiles
+        {
+            get { return allFiles; }
+            set
+            {
+                allFiles = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        private DBFileVM selectFile { get; set; }
+        public DBFileVM SelectFile 
+        {
+            get { return selectFile; }
+            set 
+            {
+                selectFile = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        public SelectUploadFileVM(List<DBProjectFileInfo> projectFileInfos) 
+        {
+            AllFiles = new ObservableCollection<DBFileVM>();
+            foreach (var item in projectFileInfos) 
+            {
+                AllFiles.Add(new DBFileVM(item));
+            }
+        }
+    }
+    class DBFileVM : NotifyPropertyChangedBase
+    {
+        public DBProjectFileInfo FileInfo { get; set; }
+        public DBFileVM(DBProjectFileInfo dBProjectFileInfo) 
+        {
+            FileInfo = dBProjectFileInfo;
+        }
+        public string Id 
+        {
+            get { return FileInfo.Id; }
+            set 
+            {
+                FileInfo.Id = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        public string Name 
+        {
+            get { return FileInfo.FileName; }
+            set 
+            {
+                FileInfo.FileName = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        public string MajorName
+        {
+            get { return FileInfo.Major; }
+            set 
+            {
+                FileInfo.Major = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        public string FileType 
+        {
+            get { return FileInfo.FileType; }
+            set
+            {
+                FileInfo.FileType = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+}

6abcd04195425c2e8e2b4c8a33ac3ea7131ea635
张珣若 <zhangxunruo@thape.com.cn>
2022-12-12 10:02:51 +0800

    deduct ifc ifc

--- THBimEngine.IO/NTS/ThNTSIfcProfileDefExtension.cs
+++ THBimEngine.IO/NTS/ThNTSIfcProfileDefExtension.cs
@@ -1,17 +1,49 @@
-﻿using System.Linq;
-using ThBIMServer.NTS;
+﻿using System;
+using System.Linq;
+
 using Xbim.Ifc2x3.ProfileResource;
-using NetTopologySuite.Geometries;
 using Xbim.Ifc2x3.GeometricConstraintResource;
+using Xbim.Ifc2x3.GeometricModelResource;
+using Xbim.Ifc2x3.SharedBldgElements;
+
+using NetTopologySuite.Geometries;
+using ThBIMServer.NTS;
 using THBimEngine.IO.Xbim;
+using Xbim.Common.Geometry;
+
 
 namespace THBimEngine.IO.NTS
 {
     public static class ThNTSIfcProfileDefExtension
     {
+        private readonly static XbimVector3D NEGATIVEZ = new XbimVector3D(0, 0, -1);
+
+        public static Polygon ToPolygon(IfcWall wall)
+        {
+            if (wall.Representation.Representations[0].Items[0] is IfcSweptAreaSolid swept)
+            {
+                var ifcProf = swept.SweptArea;
+                var localPalce = ((IfcLocalPlacement)wall.ObjectPlacement).RelativePlacement;
+
+            }
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// 应该在xbim原代码 ToxBimFace points里面去改
+        /// </summary>
+        /// <param name="profile"></param>
+        /// <param name="localPlacement"></param>
+        /// <returns></returns>
         public static Polygon ToPolygon(this IfcProfileDef profile, IfcLocalPlacement localPlacement)
         {
-            var lineString = profile.ToXbimFace(localPlacement).OuterBound.Points.ToArray().ToLineString();
+            var pointsArray = profile.ToXbimFace(localPlacement).OuterBound.Points.ToArray();
+            if (profile is IfcRectangleProfileDef)
+            {
+                pointsArray.Append(pointsArray[0]);
+            }
+
+            var lineString = pointsArray.ToLineString();
             if (lineString is LinearRing ring)
             {
                 return ThIFCNTSService.Instance.GeometryFactory.CreatePolygon(ring);
---
+++ XbimXplorer/Deduct/DeductEngineIfcVsIfc2.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xbim.Ifc2x3;
+using Xbim.Ifc2x3.GeometryResource;
+using Xbim.Ifc2x3.ProfileResource;
+using Xbim.Ifc2x3.ProductExtension;
+using Xbim.Ifc2x3.Kernel;
+using Xbim.Ifc2x3.SharedBldgElements;
+using Xbim.Ifc2x3.GeometricModelResource;
+using Xbim.Ifc2x3.GeometricConstraintResource;
+
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Mathematics;
+using NetTopologySuite.Operation.Buffer;
+using NetTopologySuite.Operation.OverlayNG;
+
+using ThBIMServer.NTS;
+using THBimEngine.Domain;
+
+namespace XbimXplorer.Deduct
+{
+    internal class DeductEngineIfcVsIfc2
+    {
+        public Xbim.Ifc.IfcStore IfcStruct;
+        public Xbim.Ifc.IfcStore IfcArchi;
+
+        //output
+        public void DeductEngine()
+        {
+            var prjStruct = IfcStruct.Instances.FirstOrDefault<IfcProject>();
+            var buildStruct = prjStruct.Sites.FirstOrDefault()?.Buildings.FirstOrDefault() as IfcBuilding;
+            var storeyStruct = buildStruct.BuildingStoreys.OfType<IfcBuildingStorey>().ToList();
+
+            var storeyWallDict = GetSpIdxIfc2(storeyStruct, out var wallTupleDict);
+            //var storeyWallDict = GetSpIdxIfc23(storeyStruct, out var wallTupleDict);
+
+          
+
+            var deductWall = DeductWall(storeyStruct, storeyWallDict);
+
+            var wallCutResult = CutBimWall(deductWall);
+
+        }
+
+        private static Dictionary<IfcBuildingStorey, ThIFCNTSSpatialIndex> GetSpIdxIfc2(List<IfcBuildingStorey> storeys, out Dictionary<string, Tuple<IfcProfileDef, IfcAxis2Placement>> wallTupleDict)
+        {
+            var storeyWallDict = new Dictionary<IfcBuildingStorey, ThIFCNTSSpatialIndex>();
+            wallTupleDict = new Dictionary<string, Tuple<IfcProfileDef, IfcAxis2Placement>>();
+            foreach (IfcBuildingStorey BuildingStorey in storeys)
+            {
+                var struProfileInfos = new List<Tuple<IfcProfileDef, IfcAxis2Placement>>();
+                foreach (var containElement in BuildingStorey.ContainsElements)
+                {
+                    var walls = containElement.RelatedElements.OfType<IfcWall>().ToList();
+                    foreach (var item in walls)
+                    {
+                        if (item.Representation.Representations[0].Items[0] is IfcSweptAreaSolid)
+                        {
+                            var profile = ((IfcSweptAreaSolid)item.Representation.Representations[0].Items[0]).SweptArea;
+                            var placement = ((IfcLocalPlacement)item.ObjectPlacement).RelativePlacement;
+                            var wTuple = Tuple.Create(profile, placement);
+                            struProfileInfos.Add(wTuple);
+                            wallTupleDict.Add(item.GlobalId, wTuple);
+                        }
+                        else
+                        {
+                            var area = item.Representation.Representations[0].Items[0];
+                        }
+
+                    }
+                }
+                var spIdx = new ThIFCNTSSpatialIndex(struProfileInfos);
+                storeyWallDict.Add(BuildingStorey, spIdx);
+            }
+
+            return storeyWallDict;
+        }
+
+
+        private static Dictionary<IfcBuildingStorey, ThIFCNTSSpatialIndex> GetSpIdxIfc23(List<IfcBuildingStorey> storeys, out Dictionary<string, Tuple<IfcProfileDef, IfcAxis2Placement>> wallTupleDict)
+        {
+            var storeyWallDict = new Dictionary<IfcBuildingStorey, ThIFCNTSSpatialIndex>();
+            wallTupleDict = new Dictionary<string, Tuple<IfcProfileDef, IfcAxis2Placement>>();
+            foreach (IfcBuildingStorey BuildingStorey in storeys)
+            {
+                var struProfileInfos = new List<Tuple<IfcProfileDef, IfcAxis2Placement>>();
+                foreach (var containElement in BuildingStorey.ContainsElements)
+                {
+                    var walls = containElement.RelatedElements.OfType<IfcWall>().ToList();
+                    foreach (var item in walls)
+                    {
+                        if (item.Representation.Representations[0].Items[0] is IfcSweptAreaSolid)
+                        {
+                            var profile = ((IfcSweptAreaSolid)item.Representation.Representations[0].Items[0]).SweptArea;
+                            var placement = ((IfcLocalPlacement)item.ObjectPlacement).RelativePlacement;
+                            var wTuple = Tuple.Create(profile, placement);
+                            struProfileInfos.Add(wTuple);
+                            wallTupleDict.Add(item.GlobalId, wTuple);
+                        }
+                        else
+                        {
+                            var area = item.Representation.Representations[0].Items[0];
+                        }
+
+                    }
+                }
+                var spIdx = new ThIFCNTSSpatialIndex(struProfileInfos);
+                storeyWallDict.Add(BuildingStorey, spIdx);
+            }
+
+            return storeyWallDict;
+        }
+
+
+
+
+        private Dictionary<string, Dictionary<THBimWall, List<Tuple<IfcProfileDef, IfcAxis2Placement>>>> DeductWall(List<IfcBuildingStorey> strucStoreys, Dictionary<IfcBuildingStorey, ThIFCNTSSpatialIndex> storeyWallDict)
+        {
+            var storeyDict = new Dictionary<string, Dictionary<THBimWall, List<Tuple<IfcProfileDef, IfcAxis2Placement>>>>();
+
+
+
+
+            //foreach (var building in IfcArchi.ProjectSite.SiteBuildings)
+            //{
+            //    foreach (var storey in building.Value.BuildingStoreys)
+            //    {
+            //        var wChange = new Dictionary<THBimWall, List<Tuple<IfcProfileDef, IfcAxis2Placement>>>();
+            //        storeyDict.Add(storey.Key, wChange);
+
+            //        var storeyItem = storey.Value;
+            //        if (storeyItem.MemoryStoreyId == string.Empty || storeyItem.MemoryStoreyId == "")
+            //        {
+            //            //标准层第一层或非标层
+            //            var sStorey = strucStoreys.Where(x =>
+            //            {
+            //                double elevation = x.Elevation.Value;
+            //                if (Math.Abs(elevation - storeyItem.Elevation) <= 50)
+            //                    return true;
+            //                else
+            //                    return false;
+            //            }).FirstOrDefault();
+
+            //            if (sStorey == null)
+            //            {
+            //                continue;
+            //            }
+            //            storeyWallDict.TryGetValue(sStorey, out var spIdx);
+
+            //            if (spIdx == null)
+            //            {
+            //                continue;
+            //            }
+            //            foreach (var w in storeyItem.FloorEntitys)
+            //            {
+            //                if (w.Value is THBimWall wallItem)
+            //                {
+            //                    var selectItem = spIdx.SelectCrossingPolygon(wallItem.GeometryParam as GeometryStretch);
+            //                    if (selectItem.Count > 0)
+            //                    {
+            //                        wChange.Add(wallItem, selectItem);
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            return storeyDict;
+
+        }
+
+
+
+        /// <summary>
+        /// key：原墙uid
+        /// value ：item1 是否只删除原墙， item2代替的新墙
+        /// 如果返回值count=0  => onlyDelete = true 只删除墙， onlyDelete = false 则保留原墙
+        /// 返回值count>0 -> 删除原墙，用新墙代替
+        /// </summary>
+        /// <param name="deductWall"></param>
+        /// <returns></returns>
+        private static Dictionary<string, Tuple<bool, List<THBimWall>>> CutBimWall(Dictionary<string, Dictionary<THBimWall, List<Tuple<IfcProfileDef, IfcAxis2Placement>>>> deductWall)
+        {
+            ////////////////////////
+            //var item = deductWall.ElementAt(0).Value.ElementAt(2);
+            //CutBimWallProcess(item);
+            ////////////////////////
+
+            var wallCutDict = new Dictionary<string, Tuple<bool, List<THBimWall>>>();
+            foreach (var storey in deductWall)
+            {
+                var wallCutPairs = storey.Value;
+                foreach (var wallCutPair in wallCutPairs)
+                {
+                    var geomArchi = ((GeometryStretch)wallCutPair.Key.GeometryParam).ToNTSPolygon();
+                    var geomStructList = wallCutPair.Value.Select(x => x.Item1.ToNTSPolygon(x.Item2)).ToList();
+
+                    var newWall = DeductService.CutBimWallGeom(geomArchi, geomStructList, out var onlyDelete);
+
+                    var newBimWall = new List<THBimWall>();
+                    if (onlyDelete == false)
+                    {
+                        newBimWall.AddRange(DeductService.ToThBimWall(wallCutPair.Key, newWall));
+                    }
+
+                    wallCutDict.Add(wallCutPair.Key.Uid, new Tuple<bool, List<THBimWall>>(onlyDelete, newBimWall));
+                }
+            }
+
+            return wallCutDict;
+        }
+
+    }
+}

76324a1f6252febeabfda3ec143c4fa9e9f53e4d
董士崇 <dongshichong@thape.com.cn>
2023-01-12 18:33:39 +0800

    Revert "Temp rollback the changes"
    This reverts commit 148412a4cce8c9fc9aab27f6b4a39db34e79e642.

---
+++ XbimXplorer/ExportViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using THBimEngine.Application;
+using THBimEngine.Domain;
+
+namespace XbimXplorer
+{
+    public class ExportViewModel : NotifyPropertyChangedBase
+    {
+        private ObservableCollection<THBimProjectViewModel> _allProjects { get; set; }
+        public ObservableCollection<THBimProjectViewModel> AllProjects
+        {
+            get { return _allProjects; }
+            set
+            {
+                _allProjects = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public ExportViewModel(THDocument document)
+        {
+            AllProjects = new ObservableCollection<THBimProjectViewModel>();
+            foreach (var project in document.AllBimProjects)
+            {
+                AllProjects.Add(new THBimProjectViewModel(project));
+            }
+        }
+    }
+
+    public class THBimProjectViewModel : NotifyPropertyChangedBase
+    {
+
+        public THBimProjectViewModel(THBimProject project)
+        {
+            Project = project;
+            ShowName = project.Name;
+            IsChecked = true;
+        }
+        private bool? _isChecked { get; set; }
+        public bool? IsChecked
+        {
+            get
+            {
+                return _isChecked;
+            }
+            set
+            {
+                _isChecked = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        public THBimProject Project { get; }
+        public string ShowName { get; }
+    }
+}
